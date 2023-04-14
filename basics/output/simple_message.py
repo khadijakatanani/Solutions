5a3ea3de0e04abedfddadb43f135c791e699fe65
Khadija Katanani <khadija.katanani@gmail.com>
2023-04-14 03:37:21 +0100

    Added code to display a simple message
    Signed-off-by: khadijakatanani <khadija.katanani@gmail.com>

---
+++ basics/output/simple_message.py
@@ -0,0 +1,14 @@
+# print ()
+
+print("Hello World")
+print("My name is Khadija")
+
+# "#", they should be descriptive/informative messages
+
+print("Hello everybody") # greeting people, in-line comment
+
+# block comment is defined when your comment starts on a new line
+
+# multiply the number of cakes with the number of guests
+print(2*8)
+
